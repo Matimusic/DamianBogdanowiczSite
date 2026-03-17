b2c43d52f5765bba0adb3b289349f7c1abfd03ed
Mati <matiepol9@gmail.com>
2026-03-17 01:16:13 +0100

    poprawa nextConfig

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export', // To generuje statyczne pliki HTML
-  basePath: '/DamianBogdanowiczSite', // TUTAJ wpisz dokładnie nazwę swojego repo na GitHubie
+  
   images: {
     unoptimized: true, // GitHub Pages nie wspiera automatycznej optymalizacji zdjęć Next.js
   },
